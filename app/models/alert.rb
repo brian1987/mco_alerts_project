df16b13a1e55251494ea52d380e2a4c7210d4c3a
brian1987 <pisano.b@gmail.com>
2014-11-19 11:39:07 -0500

    added notes to model

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -6,6 +6,25 @@ class Alert < ActiveRecord::Base
     end
   end
 
+
+  # Should I create a new model to send alert message. Text below establishes
+  # connection w/ Stagin in GSI
+  # client = Savon.client(wsdl: "https://staging.gsihealth.com:8083/NotificationManager-1.0/NotificationConsumer?wsdl")
+  # <Savon::Client:0x000001040d5840 @globals=#<Savon::GlobalOptions:0x000001040d5818 
+  # @option_type=:global, @options={:encoding=>"UTF-8", :soap_version=>1, 
+  # :namespaces=>{}, :logger=>#<Logger:0x000001040d5778 @progname=nil, 
+  # @level=0, @default_formatter=#<Logger::Formatter:0x000001040d56d8 
+  # @datetime_format=nil>, @formatter=nil, @logdev=#<Logger::LogDevice:0x000001040d5688 @shift_size=nil, 
+  # @shift_age=nil, @filename=nil, @dev=#<IO:/dev/null>, @mutex=#<Logger::LogDevice::LogDeviceMutex:0x000001040d5638 
+  # @mon_owner=nil, @mon_count=0, @mon_mutex=#<Mutex:0x000001040d55e8>>>>, 
+  # :log=>true, :filters=>[], :pretty_print_xml=>false, :raise_errors=>true, 
+  # :strip_namespaces=>true, :convert_response_tags_to=>
+  #<Proc:0x000001040d5548@/usr/local/rvm/gems/ruby-2.0.0-p353/gems/savon-2.3.3/lib/savon/options.rb:56 (lambda)>, 
+  # :multipart=>false, :wsdl=>"https://staging.gsihealth.com:8083/NotificationManager-1.0/NotificationConsumer?wsdl"}>, 
+  # @wsdl=#<Wasabi::Document:0x000001040d5160 @document="https://staging.gsihealth.com:8083/NotificationManager-1.0/NotificationConsumer?wsdl", 
+  # @request=#<HTTPI::Request:0x000001040d5070>>> 
+
+
   #Above worked just to import CSV
   #how am i going to send this stuff
   #Testing new import method below
